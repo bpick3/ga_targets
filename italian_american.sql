63a32e8d059c5e700cb44efb72917ec5203af938
Brandon Pickett <63067820+bpick3@users.noreply.github.com>
2024-10-02 16:12:15 -0600

    Create italian_american.sql

---
+++ italian_american.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE TABLE `stac-labs.ga_sharing.italian_american_targets` AS
+SELECT
+    ts.*,
+    p.sos_id,
+    p.myv_van_id,
+    p.first_name,
+    p.middle_name,
+    p.last_name,
+    p.suffix,
+    p.age_combined,
+    p.date_of_birth_combined,
+    p.gender_vf,
+    p.ethnicity_combined,
+    p.ethnicity_modeled_confidence_level,
+    p.county_name,
+    p.voting_street_address,
+    p.voting_street_address_2,
+    p.voting_zip,
+    p.voting_city,
+    p.primary_phone_number,
+    p.us_cong_district_latest,
+    p.state_house_district_latest,
+    p.state_senate_district_latest,
+    s.support,
+    s.turnout
+FROM `democrats.consumer_ga.tsmart_ethnicity_plus` ts
+LEFT JOIN `democrats.analytics_ga.person` p
+    ON ts.person_id = p.person_id
+LEFT JOIN `democrats.scores_ga.current_scores` s
+    ON ts.person_id = s.person_id
+WHERE ts.sub_ethnicity IN ('Italian')
+AND p.person_id IS NOT NULL
+AND p.is_deceased = false
+AND p.ethnicity_modeled_confidence_level IN ('Medium','High')
